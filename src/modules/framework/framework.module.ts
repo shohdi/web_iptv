7edfb7d2baf34cf7a851974e2c28d1de8791048f
Shohdy ELSheemy <shohdi@gmail.com>
2022-12-02 22:38:52 +0300

    add swal and loading

--- src/modules/framework/framework.module.ts
+++ src/modules/framework/framework.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { LoadingComponent } from './components/loading/loading.component';
 import {DbService} from './services/db-service';
 
 
 
 @NgModule({
   declarations: [
-    
+    LoadingComponent
   ],
   imports: [
     
-    
+    SweetAlert2Module 
   ],
   providers: [
     DbService
+  ],
+  exports:[
+    LoadingComponent
   ]
  
 })
